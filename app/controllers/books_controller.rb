8e30190f8db5cdd62953cfc6bf7fc3f23b9ea710
EC2 Default User <ec2-user@ip-172-31-13-224.ap-northeast-1.compute.internal>
2021-03-10 03:41:05 +0000

    commit3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-     flash[:notice] = "succesfully"
-     redirect_to book_path(book.id)
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
       
     else
       @books=Book.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'books#top'
+  
   resources:books
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
